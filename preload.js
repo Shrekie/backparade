6e32f4fc16a82025f2572c5ae3b4fabf132cdc4c
shrekie <16154821+Shrekie@users.noreply.github.com>
2021-07-28 23:20:02 +0200

    Get a directory with names and other metadata

--- preload.js
+++ preload.js
@@ -1,12 +1,32 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
-})
+const { testPath } = require("./private.js");
+const { readdir, stat } = require("fs/promises");
+
+// All file names from a directory
+readdir(testPath).then((names) => {
+  Promise.all(
+    // Metadata promise each file in directory by name
+    names.map((name) => stat(`${testPath}/${name}`))
+  ).then((metadata) => {
+    // Merge name with metadata
+    files = names.map((name, index) => ({ name, ...metadata[index] }));
+
+    // Play
+    files.forEach((file) => {
+      const field = document.createElement("p");
+      field.innerHTML = file.name;
+
+      const thumbnail = document.createElement("img");
+      thumbnail.src = `${testPath}/${file.name}`;
+      thumbnail.width = 50;
+      thumbnail.height = 50;
+
+      document.getElementById("directory-list").appendChild(field);
+      document.getElementById("directory-list").appendChild(thumbnail);
+    });
+  });
+});
+
+window.addEventListener("DOMContentLoaded", () => {});
